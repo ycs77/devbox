b3f14271ef9570d982ebf076ed12750db7fbe520
Lucas Yang <yangchenshin77@gmail.com>
2026-08-06 15:09:04 +0800

    Add a buildable Node 24.19.0 image (#2)

---
+++ scripts/build-node-image.mjs
@@ -0,0 +1,20 @@
+import { spawn } from 'node:child_process'
+import { once } from 'node:events'
+import { fileURLToPath } from 'node:url'
+
+const image = 'devbox-node:24.19.0'
+const projectRoot = fileURLToPath(new URL('../', import.meta.url))
+const dockerfile = fileURLToPath(new URL('../Dockerfile.node', import.meta.url))
+
+const build = spawn('docker', ['build', '--file', dockerfile, '--tag', image, '.'], {
+  cwd: projectRoot,
+  stdio: 'inherit',
+})
+
+const [code] = await once(build, 'close')
+
+if (code !== 0) {
+  process.exitCode = code ?? 1
+} else {
+  console.log(image)
+}
