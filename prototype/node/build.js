4981caf90cd260e8506b8df87aaa9b4ca21180f5
Lucas Yang <yangchenshin77@gmail.com>
2026-08-14 16:52:54 +0800

    Prepare Node 24 prototype for generated Devbox images

--- prototype/node/build.js
+++ prototype/node/build.js
@@ -1,10 +1,13 @@
+// Build the prototype image with:
+// docker build --file prototype/node/Dockerfile --tag devbox:latest .
+//
 // Enter the prototype image with:
-// docker run --rm -it --entrypoint /bin/bash devbox-node:24.19.0
+// docker run --rm -it --user devbox -e NODE_VERSION=24 devbox:latest /bin/bash
 import { spawn } from 'node:child_process'
 import { once } from 'node:events'
 import { fileURLToPath } from 'node:url'
 
-const image = 'devbox-node:24.19.0'
+const image = 'devbox:latest'
 const projectRoot = fileURLToPath(new URL('../../', import.meta.url))
 const dockerfile = fileURLToPath(new URL('./Dockerfile', import.meta.url))
 
